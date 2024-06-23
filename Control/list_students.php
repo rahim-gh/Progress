472b87c59beb7e2542ace14d3f5a4f7aba9ff67e
rahim-gh <rahimguerrouf0@gmail.com>
2024-06-23 14:01:15 +0100

    - improve style (css) - fix issues in navbar

--- Control/list_students.php
+++ Control/list_students.php
@@ -5,7 +5,7 @@ $query = "SELECT * FROM students_db";
 $result = mysqli_query($con, $query);
 
 while ($row = mysqli_fetch_assoc($result)) {
-    echo "<tr class=\"table_content\">";
+    echo "<tr class='table_content'>";
     echo "<td>". $row['ID']. "</td>";
     echo "<td>". $row['NAME']. "</td>";
     echo "<td>". $row['FAMILYNAME']. "</td>";
